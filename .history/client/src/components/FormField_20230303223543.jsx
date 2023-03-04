e042c61fe468be2c3cb3ef212a9b9a5e428fa4d9
wragyu <rfgalang196@gmail.com>
2023-03-03 22:38:52 -0500

    Progress: Create-Post UI fully done. Input field not working

--- .history/client/src/components/FormField_20230303223543.jsx
+++ .history/client/src/components/FormField_20230303223543.jsx
@@ -29,7 +29,8 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         onChange={handleChange}
         required
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
-      />
+      ?>
+      </input>
     </div>
   )
 }
